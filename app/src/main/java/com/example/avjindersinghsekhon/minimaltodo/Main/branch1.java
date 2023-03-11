203222d6acab89b1f2b4069477f7486c853b874e
unknown <urmi1907094@stud.kuet.ac.bd>
2023-03-11 23:29:41 +0600

    new class for conflict

---
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/Main/branch1.java
@@ -0,0 +1,8 @@
+package com.example.avjindersinghsekhon.minimaltodo.Main;
+
+public class branch1 {
+    public static void main(String args[]) {
+        System.out.print("Hello!");
+        System.out.print("Urmi");
+    }
+}
